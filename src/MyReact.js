d0a11c5329ac73f6f74e81fa38523d2660248000
unk-pn <unkpn.dev@gmail.com>
2026-08-14 13:55:34 +0300

    feat: concurrent mode

--- src/MyReact.js
+++ src/MyReact.js
@@ -78,3 +78,19 @@ function render(element, container) {
 
   container.appendChild(dom)
 }
+
+let nextUnitOfWork = null
+function workLoop(deadline) {
+  let shouldYield = false
+  while (nextUnitOfWork && !shouldYield) {
+    nextUnitOfWork = performUnitOfWork(nextUnitOfWork)
+    shouldYield = deadline.timeRemaining < 1
+  }
+  requestIdleCallback(workLoop)
+}
+
+requestIdleCallback(workLoop)
+
+function performUnitOfWork(nextUnitOfWork) {
+  // TODO
+}
